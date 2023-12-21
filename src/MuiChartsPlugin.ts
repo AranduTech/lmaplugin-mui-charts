887859bb1c2343beb74836b9a2b8a4dd7715004d
Bruno Polo <brunopolo@poli.ufrj.br>
2023-12-21 17:57:31 -0300

    Update package version and add new widget components

--- src/MuiChartsPlugin.ts
+++ src/MuiChartsPlugin.ts
@@ -1,10 +1,24 @@
 
+import { addFilter } from '@arandu/laravel-mui-admin';
+
 import { LaravelMuiAdminPlugin } from '@arandu/laravel-mui-admin/lib/types/plugin';
+import { WidgetProps } from '@arandu/laravel-mui-admin/lib/components/Widgets/Widget';
+
+import BarsWidget from './components/BarsWidget';
+import LineWidget from './components/LineWidget';
+
+type WidgetTypeMap = { 
+    [key: string]: (props: WidgetProps) => JSX.Element 
+};
 
 const MuiChartsPlugin: LaravelMuiAdminPlugin = {
     macros: () => {
-        console.log('Plugin being loaded...');
 
+        addFilter('widget_type_component_map', (map: WidgetTypeMap) => ({
+            ...map,
+            line: LineWidget,
+            bars: BarsWidget,
+        }));
     },
 
 };
